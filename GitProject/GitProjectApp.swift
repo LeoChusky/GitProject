96c88fbdc6d7af5e0002367e6bc21f3393d1af30
Apprenant 007 <apprenant007@MacBook-Pro-9.local>
2022-06-15 13:56:31 +0200

    First commit

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Apprenant 007 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
